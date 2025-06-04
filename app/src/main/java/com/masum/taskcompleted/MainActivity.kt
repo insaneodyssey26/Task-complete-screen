a0da9c37fea3254c8f6fa7230b6a24e8527728f9
Masum <hydrapikachu29@gmail.com>
2025-06-04 15:18:52 +0530

    Added image resource as painter

--- app/src/main/java/com/masum/taskcompleted/MainActivity.kt
+++ app/src/main/java/com/masum/taskcompleted/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.masum.taskcompleted.ui.theme.TaskCompletedTheme
@@ -28,14 +30,16 @@ class MainActivity : ComponentActivity() {
 fun TaskCompleted() {
     Screen(
         title = stringResource(R.string.all_tasks_completed),
-        greetings = stringResource(R.string.nice_work)
+        greetings = stringResource(R.string.nice_work),
+        confirmation = painterResource(R.drawable.ic_task_completed)
     )
 }
 
 @Composable
 fun Screen(
     title: String,
-    greetings: String
+    greetings: String,
+    confirmation: Painter
 ) {
 
 }
