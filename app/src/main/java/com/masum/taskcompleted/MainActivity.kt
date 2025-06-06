ee223f98fa8004efe5ded0a50189a51c5e403e5c
Masum <hydrapikachu29@gmail.com>
2025-06-06 23:31:42 +0530

    Removed unused imports

--- app/src/main/java/com/masum/taskcompleted/MainActivity.kt
+++ app/src/main/java/com/masum/taskcompleted/MainActivity.kt
@@ -10,16 +10,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.masum.taskcompleted.ui.theme.TaskCompletedTheme
