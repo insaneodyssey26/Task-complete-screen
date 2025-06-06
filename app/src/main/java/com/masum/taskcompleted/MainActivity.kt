23647b3f1391e44ef56225449d77257267cfd22a
Masum <hydrapikachu29@gmail.com>
2025-06-06 20:54:24 +0530

    Added column logic

--- app/src/main/java/com/masum/taskcompleted/MainActivity.kt
+++ app/src/main/java/com/masum/taskcompleted/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
@@ -26,4 +30,15 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@Composable
+fun MainScreen() {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
+    }
+}
