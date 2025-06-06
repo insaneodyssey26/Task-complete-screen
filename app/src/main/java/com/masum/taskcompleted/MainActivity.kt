45fdb27f87553e4550d533067fdfeb79c3a45d30
Masum <hydrapikachu29@gmail.com>
2025-06-06 20:40:21 +0530

    Deleted previous implementation type

--- app/src/main/java/com/masum/taskcompleted/MainActivity.kt
+++ app/src/main/java/com/masum/taskcompleted/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -21,8 +22,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TaskCompleted()
-            }
+
+        }
         }
     }
 
