8926695a2441781b0dbba22921754ddb82fdc6ac
Masum <hydrapikachu29@gmail.com>
2025-06-06 23:30:20 +0530

    Added second text and fixed alignments with surface

--- app/src/main/java/com/masum/taskcompleted/MainActivity.kt
+++ app/src/main/java/com/masum/taskcompleted/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.masum.taskcompleted.ui.theme.TaskCompletedTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,6 +54,10 @@ fun MainScreen() {
         Text(
             text = stringResource(R.string.all_tasks_completed),
             modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = stringResource(R.string.nice_work),
+            fontSize = 16.sp,
         )
     }
 }
